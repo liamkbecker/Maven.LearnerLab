9a9928f32e3c8d98cb81fdc69befa5081404c6d0
hazel <liam.k.becker@gmail.com>
2021-03-23 17:16:37 -0400

    Part 1 done

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,24 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+    final long id;
+    String name;
+
+    public Person(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
 }
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,37 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void testConstructor(){
+
+        String givenName = "uwu bean";
+        long givenId = 1000;
+        Person person = new Person(givenId, givenName);
+
+        String retrievedName = person.getName();
+        long retrievedId = person.getId();
+
+        Assert.assertEquals(givenId, retrievedId);
+        Assert.assertEquals(givenName, retrievedName);
+
+    }
+
+    @Test
+    public void testSetName(){
+
+        Person person = new Person(234, "aaa");
+        String givenName = "bbb";
+        person.setName(givenName);
+
+        String retrievedName = person.getName();
+
+        Assert.assertEquals(givenName, retrievedName);
+
+    }
+
 
 }
